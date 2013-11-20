1f5e72ee23e07a08e2b5a013c7522956c3f2b176
Kyle Neumeier <neumeier@careevolution.com>
2013-11-19 21:11:58 -0500

    Answer to FizzBuzz in Python

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+import sys
+
+for num in range(1,101):
+	if(num % 3 == 0):
+		sys.stdout.write("Fizz")
+	if(num % 5 ==0 ):
+		sys.stdout.write("Buzz")
+	if((num % 3!=0) and (num %5 !=0)):
+		print num,
+	print ""
